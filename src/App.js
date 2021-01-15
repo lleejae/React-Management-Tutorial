3f38db8fa63ef0e292ab1438ae5cf4e3bb456b0e
lleejae <dlwogur5020@naver.com>
2021-01-16 01:28:22 +0900

    ㅇ음

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h2>go to Development!</h2>
+    </div>
+  );
+}
+
+export default App;
